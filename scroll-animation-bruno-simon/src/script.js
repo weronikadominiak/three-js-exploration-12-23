9c5150e1a9cf7905d3682f5487c853a775643d09
weronikadominiak <mail@weronika.dev>
2023-12-12 12:01:28 +0000

    feat: small cleanup and css adjustments

--- scroll-animation-bruno-simon/src/script.js
+++ scroll-animation-bruno-simon/src/script.js
@@ -66,7 +66,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true, // make canvas transparent
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
